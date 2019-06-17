2dd3c8a28cf163a1ee6a65abe4ae4a1eee42909b
brbitway <50849441+brbitway@users.noreply.github.com>
2019-06-17 15:13:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+var game = [[0,0,0],[0,0,0],[0,0,0]];
+
+var vez = "x";
+
+var jogadas = 0;
+
+var div = document.getElementById("jogo");
+
+div.addEventListener("click",listener);
+
+function listener(event) {
+
+	var id = event.target.id;
+
+	if(id[0] != "c"){return ;}
+
+	var i1 = id[2];
+
+	var i2 = id[4];
+
+	game[i1][i2] = vez;
+
+	colocaImg(event.target,"imgs/" + vez + ".jpg",200,200);
+
+	var r = win(vez);
+
+	if(r == "x"){
+
+		alert("X ganhou");
+
+	}else if(r == "o"){
+
+		alert("O ganhou");
+
+	}else if(jogadas > 8){
+
+		alert("Empatou");
+
+	}
+
+	if(vez == "x"){
+
+		vez = "o";
+
+	}else{
+
+		vez = "x";
+
+	}
+
+
+}
+
+function win(player) {
+	//linhas
+	
+	if(game[0][0] == game[0][1] && game[0][0] == game[0][2] && game[0][0]==player ){
+		return game[0][0];
+	}else if(game[1][0] == game[1][1] && game[1][0] == game[1][2] && game[1][0]==player ){
+		return game[1][0];
+	}else if(game[2][0] == game[2][1] && game[2][0] == game[2][2] && game[2][0]==player ){
+		return game[2][0];
+	//Colunas
+
+	}else if(game[0][0] == game[1][0] && game[0][0] == game[2][0] && game[0][0]==player ){
+		return game[0][0];
+	}else if(game[1][0] == game[1][1] && game[1][0] == game[1][2] && game[1][0]==player ){
+		return game[1][0];
+	}else if(game[2][0] == game[2][1] && game[2][0] == game[2][2] && game[2][0]==player ){
+		return game[2][0];
+	//Diagonais
+
+	}else if(game[0][2] == game[1][1] && game[0][2] == game[2][0] && game[0][2]==player ){
+		return game[0][2];
+	}else if(game[0][0] == game[1][1] && game[0][0] == game[2][2] && game[0][0]==player ){
+		return game[0][0];
+	}else{
+		return 0;
+	}
+}
+
+function colocaImg(father,src,h,w){
+	var oImg = document.createElement("img");
+	oImg.setAttribute('src', src);
+	oImg.setAttribute('alt', 'na');
+	oImg.setAttribute('height', h);
+	oImg.setAttribute('width', w);
+	father.appendChild(oImg);
+}
